4e11bb1e57b7bd23264f28bfa5c786c47fb3859f
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2025-02-12 11:10:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,25 +1,29 @@
 #define GL_SILENCE_DEPRECATION
 #define GL_GLEXT_PROTOTYPES 1
-#define LOG(argument) std::cout << argument << '\n'
 
 #ifdef _WINDOWS
 #include <GL/glew.h>
 #endif
+
+#define GL_SILENCE_DEPRECATION
+#define STB_IMAGE_IMPLEMENTATION
+#define LOG(argument) std::cout << argument << '\n'
+#define GL_GLEXT_PROTOTYPES 1
 
 #include <SDL.h>
 #include <SDL_opengl.h>
 #include "glm/mat4x4.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 #include "ShaderProgram.h"
+#include "stb_image.h"
 
 enum AppStatus { RUNNING, TERMINATED };
 
-constexpr char V_SHADER_PATH[] = "shaders/vertex.glsl",
-               F_SHADER_PATH[] = "shaders/fragment.glsl";
-
+// Window dimensions
 constexpr int WINDOW_WIDTH  = 640,
               WINDOW_HEIGHT = 480;
 
+// Background color components
 constexpr float BG_RED     = 0.1922f,
                 BG_BLUE    = 0.549f,
                 BG_GREEN   = 0.9059f,
@@ -30,66 +34,118 @@ constexpr int VIEWPORT_X      = 0,
               VIEWPORT_WIDTH  = WINDOW_WIDTH,
               VIEWPORT_HEIGHT = WINDOW_HEIGHT;
 
-constexpr int TRIANGLE_RED     = 1.0,
-              TRIANGLE_BLUE    = 0.4,
-              TRIANGLE_GREEN   = 0.4,
-              TRIANGLE_OPACITY = 1.0;
+constexpr char V_SHADER_PATH[] = "shaders/vertex.glsl",
+               F_SHADER_PATH[] = "shaders/fragment.glsl";
 
-SDL_Window* g_display_window;
+constexpr float TRIANGLE_RED     = 1.0,
+                TRIANGLE_BLUE    = 0.4,
+                TRIANGLE_GREEN   = 0.4,
+                TRIANGLE_OPACITY = 1.0;
+
+constexpr GLint NUMBER_OF_TEXTURES = 1,
+                LEVEL_OF_DETAIL    = 0,
+                TEXTURE_BORDER     = 0;
+
+constexpr char HELI_FILEPATH[]    = "helicopter.png",
+               CAR_FILEPATH[] = "car.png";
+
+constexpr glm::vec3 INIT_SCALE_HELI = glm::vec3(5.0f, 5.0f, 0.0f),
+                    INIT_SCALE_CAR = glm::vec3(5.0f, 5.0f, 0.0f),
+                    INIT_POS_HELI = glm::vec3(2.0f, 0.0f, 0.0f),
+                    INIT_POS_CAR = glm::vec3(-2.0f, 0.0f, 0.0f);
+
+constexpr float ROT_INCREMENT = 1.0f;
 
 AppStatus g_app_status = RUNNING;
-
-int  g_frame_counter = 0;
+SDL_Window* g_display_window;
 
 ShaderProgram g_shader_program;
+
 glm::mat4 g_view_matrix,
-          g_model_matrix,
+          g_heli_matrix,
+          g_car_matrix,
           g_projection_matrix;
 
-// ——————————— GLOBAL VARS AND CONSTS FOR TRANSFORMATIONS ——————————— //
+glm::vec3 g_heli_translation  = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 g_car_translation  = glm::vec3(0.0f, 0.0f, 0.0f);
 
-// —————————————————————————————————————————————————————————————————— //
+float g_previous_ticks = 0.0f;
 
+glm::vec3 g_rotation_car    = glm::vec3(0.0f, 0.0f, 0.0f);
+
+GLuint g_heli_texture_id,
+       g_car_texture_id;
+
+GLuint load_texture(const char* filepath)
+{
+    int width, height, number_of_components;
+    unsigned char* image = stbi_load(filepath, &width, &height, &number_of_components, STBI_rgb_alpha);
+
+    if (image == NULL)
+    {
+        LOG("Unable to load image. Make sure the path is correct.");
+        assert(false);
+    }
+
+    GLuint textureID;
+    glGenTextures(NUMBER_OF_TEXTURES, &textureID);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+    glTexImage2D(GL_TEXTURE_2D, LEVEL_OF_DETAIL, GL_RGBA, width, height, TEXTURE_BORDER, GL_RGBA, GL_UNSIGNED_BYTE, image);
+
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+    stbi_image_free(image);
+
+    return textureID;
+}
 
 void initialise()
 {
     SDL_Init(SDL_INIT_VIDEO);
-    g_display_window = SDL_CreateWindow("Transformation Exercise",
-        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-        WINDOW_WIDTH, WINDOW_HEIGHT,
-        SDL_WINDOW_OPENGL);
-
+    g_display_window = SDL_CreateWindow("2D Scene",
+                                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                                      WINDOW_WIDTH, WINDOW_HEIGHT,
+                                      SDL_WINDOW_OPENGL);
+    
+    if (g_display_window == nullptr)
+    {
+        std::cerr << "ERROR: SDL Window could not be created.\n";
+        g_app_status = TERMINATED;
+        
+        SDL_Quit();
+        exit(1);
+    }
+    
     SDL_GLContext context = SDL_GL_CreateContext(g_display_window);
     SDL_GL_MakeCurrent(g_display_window, context);
-
+    
 #ifdef _WINDOWS
     glewInit();
 #endif
-
+    
     glViewport(VIEWPORT_X, VIEWPORT_Y, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
-
+    
     g_shader_program.load(V_SHADER_PATH, F_SHADER_PATH);
-
+    
     g_view_matrix       = glm::mat4(1.0f);
-    g_model_matrix      = glm::mat4(1.0f);
+    g_heli_matrix     = glm::mat4(1.0f);
+    g_car_matrix     = glm::mat4(1.0f);
     g_projection_matrix = glm::ortho(-5.0f, 5.0f, -3.75f, 3.75f, -1.0f, 1.0f);
-
+    
     g_shader_program.set_projection_matrix(g_projection_matrix);
     g_shader_program.set_view_matrix(g_view_matrix);
-
-    g_shader_program.set_colour(TRIANGLE_RED, 
-                                TRIANGLE_BLUE,
-                                TRIANGLE_GREEN,
-                                TRIANGLE_OPACITY);
-
+        
     glUseProgram(g_shader_program.get_program_id());
+    
+    glClearColor(BG_RED, BG_BLUE, BG_GREEN, BG_OPACITY);
+    
+    g_heli_texture_id    = load_texture(HELI_FILEPATH);
+    g_car_texture_id = load_texture(CAR_FILEPATH);
 
-    glClearColor(BG_RED, 
-                 BG_BLUE,
-                 BG_GREEN,
-                 BG_OPACITY);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
-
 
 void process_input()
 {
@@ -103,51 +159,79 @@ void process_input()
     }
 }
 
-
-void update()
-{
-    // ——————————— YOUR ORBIT TRANSFORMATIONS SHOULD GO HERE ——————————— //
+void update() {
     
-    // ————————————————————————————————————————————————————————————————— //
+    float ticks = (float) SDL_GetTicks() / 1000.0; // Seconds since start
+    float delta_time = ticks - g_previous_ticks; // Seconds since the last frame
+    g_previous_ticks = ticks;
+
+    g_heli_translation.x += 1.0f * delta_time;
+    g_heli_translation.y += 1.0f * delta_time;
+    g_car_translation.x += 1.3f * delta_time;
+    g_car_translation.y += 1.3f * delta_time;
+    
+    g_heli_matrix    = glm::mat4(1.0f);
+    g_car_matrix    = glm::mat4(1.0f);
+    
+    g_heli_matrix = glm::translate(g_heli_matrix, glm::vec3(cos(g_heli_translation.x), sin(g_heli_translation.y), 0.0f)); // Now translate the identity matrix
+    g_car_matrix = glm::translate(g_heli_matrix, glm::vec3(cos(g_car_translation.x), sin(g_car_translation.y), 0.0f));
 }
 
+void draw_object(glm::mat4 &object_g_model_matrix, GLuint &object_texture_id)
+{
+    g_shader_program.set_model_matrix(object_g_model_matrix);
+    glBindTexture(GL_TEXTURE_2D, object_texture_id);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
 
 void render() {
     glClear(GL_COLOR_BUFFER_BIT);
-
-    g_shader_program.set_model_matrix(g_model_matrix);
-
+    
     float vertices[] =
     {
-         0.5f, -0.5f,
-         0.0f,  0.5f,
-        -0.5f, -0.5f
+        -0.5f, -0.5f, 0.5f, -0.5f, 0.5f, 0.5f,  // triangle 1
+        -0.5f, -0.5f, 0.5f, 0.5f, -0.5f, 0.5f   // triangle 2
     };
 
-    glVertexAttribPointer(g_shader_program.get_position_attribute(), 
-                          2, GL_FLOAT, false, 0, vertices);
+    // Textures
+    float texture_coordinates[] =
+    {
+        0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 0.0f,     // triangle 1
+        0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 0.0f,     // triangle 2
+    };
+
+    glVertexAttribPointer(g_shader_program.get_position_attribute(), 2, GL_FLOAT, false,
+                          0, vertices);
     glEnableVertexAttribArray(g_shader_program.get_position_attribute());
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glVertexAttribPointer(g_shader_program.get_tex_coordinate_attribute(), 2, GL_FLOAT,
+                          false, 0, texture_coordinates);
+    glEnableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
+
+    // Bind texture
+    draw_object(g_heli_matrix, g_heli_texture_id);
+    draw_object(g_car_matrix, g_car_texture_id);
+
+    // We disable two attribute arrays now
     glDisableVertexAttribArray(g_shader_program.get_position_attribute());
+    glDisableVertexAttribArray(g_shader_program.get_tex_coordinate_attribute());
 
     SDL_GL_SwapWindow(g_display_window);
 }
 
-
 void shutdown() { SDL_Quit(); }
-
 
 int main(int argc, char* argv[])
 {
     initialise();
-
+    
     while (g_app_status == RUNNING)
     {
         process_input();
         update();
         render();
     }
-
+    
     shutdown();
     return 0;
 }
